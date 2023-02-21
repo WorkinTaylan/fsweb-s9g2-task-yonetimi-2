15fdae3afca026ab6689a9b2a492a245eaf3f068
WorkinTaylan <acikgozta@gmail.com>
2023-02-22 01:54:32 +0300

    proje son hali

--- src/Task.js
+++ src/Task.js
@@ -4,22 +4,18 @@ import { formatDistance } from "date-fns";
 
 const Task = ({ taskObj, onComplete }) => {
 
-const result=differenceInDays(
-  new Date(),
+const dateDiffer=differenceInDays(
   new Date(taskObj.deadline),
+  new Date(),
   )
+//console.log(dateDiffer)
+  const result=Math.abs(dateDiffer)
 
-  const result2=formatDistance(
-    new Date(taskObj.deadline), 
-    new Date(), {
-      addSuffix:true
-    }
-  )
  
   return (
     <div className="p-6 bg-[#fff] rounded-[5px] leading-20 mt-1 shadow-[0_4px_5px_rgb(0,0,0,0.1)]">
       <h3 className="text-[18px] text-[#c8781a] ">{taskObj.title}</h3>
-      <div className="text-xm pt-4">son teslim: <span className="px-[3px] py-[8px] border-solid border rounded-[2px] bg-[rgb(255,141,7)] inline-block">{result>0?result+" gün geçti":result*-1 + " gün kaldı"}</span></div>
+      <div className="text-xm pt-4">son teslim: <span className={`px-[3px] py-[8px] border-solid border rounded-[3px] inline-block ${dateDiffer<=3?"bg-[#ffd9d4]":"bg-[#137ffaf7]"}`}>{dateDiffer>0?dateDiffer+" gün kaldı":result+ " gün geçti"}</span></div>
       <p className="text-[#444] text-[14px] px-[8px] py-[12px]">{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
